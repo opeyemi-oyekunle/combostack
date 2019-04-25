f41610995f528020e42bfabf006f053ed5fdd59e
Opeyemi Oyekunle <opyzyle1960@gmail.com>
2019-04-25 02:21:25 +0100

    server.js

--- server.js
+++ server.js
@@ -11,13 +11,15 @@ app.get('/api/customers', (req, res)=>{
   res.json(customers);
 })
 
-if(process.env.NODE_ENV === 'production'){
-	app.use(express.static('build/client'))
-  app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  })
-}
-
 
 const port = process.env.PORT || 5000;
-app.listen(port, ()=> console.log(`Server started on port ${port}`));
+app.listen(port, ()=> {
+  console.log(`Server started on port ${port}`));
+  if(process.env.NODE_ENV === 'production'){
+  	app.use(express.static('build/client'))
+    app.get('*', (req,res)=>{
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+  }
+
+}

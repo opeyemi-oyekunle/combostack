54db7b10e660281f62552741c136ae1b648e93e1
Opeyemi Oyekunle <opyzyle1960@gmail.com>
2019-04-25 09:00:15 +0100

    hjfkjdfk

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/api/customers', (req, res)=>{
 })
 
 if(process.env.NODE_ENV === 'production'){
-	app.use(express.static('build/client'))
+	app.use(express.static('client/build'))
   app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   })
